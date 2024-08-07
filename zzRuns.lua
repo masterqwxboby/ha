f27c09d21d41a278aec8f6bfa2de64e3daea5b2a
masterqwxboby <167503671+masterqwxboby@users.noreply.github.com>
2024-08-06 20:58:41 -0700

    Update zzRuns.lua

--- zzRuns.lua
+++ zzRuns.lua
@@ -1,17 +1,17 @@
+
 local Players = game:GetService("Players")
 
 local function highlight(player)
-  local highLight = Instance.new("Highlight")
-  highLight.Parent = player.Character
-  highLight.FillColor = Color3.fromRGB(255,0,0)
-  highLight.OutLineColor = Color3.fromRGB(255,0,0)
+	local highLight = Instance.new("Highlight")
+	highLight.Parent = player.Character
+	highLight.FillColor = Color3.fromRGB(255,0,0)
+	highLight.OutLineColor = Color3.fromRGB(255,0,0)
 end
 
 
 Players.PlayerAdded:Connect(function(player)
-    player.CharacterAdded:Connect(function(character)
-        wait(1)
-        hightlight(player)
-     end)
- end)
-  
+	player.CharacterAdded:Connect(function(character)
+		wait(1)
+		highlight(player)
+	end)
+end)
